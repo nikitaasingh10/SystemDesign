b69a6ea9f9d2db3c2fa6754518a1882587731caa
Nikita Singh <nikita.singh.10964@gmail.com>
2025-12-28 20:35:03 +0530

    Added middleware to calculate latency

--- high-performance-user-API/src/app.js
+++ high-performance-user-API/src/app.js
@@ -4,6 +4,18 @@ import userRouter from './routes/user.routes.js';
 const app = express();
 app.use(express.json());
 
+// response time middleware
+app.use((req, res, next) => {
+    const start = Date.now();
+
+    res.on('finish', () => {
+        const duration = Date.now() - start;
+        console.log(`${req.method} ${req.path} ${duration}ms`);
+    });
+
+    next();
+});
+
 app.get('/health', (req, res) => {
     res.json({status: "UP"})
 });
