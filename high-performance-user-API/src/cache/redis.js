81721d6c648a58c4aa1c812f79eb799038d2ef5f
Nikita Singh <nikita.singh.10964@gmail.com>
2025-12-28 20:43:29 +0530

    Adding redis-client

---
+++ high-performance-user-API/src/cache/redis.js
@@ -0,0 +1,16 @@
+import {createClient} from "redis";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const redisClient = createClient({
+    url: process.env.REDIS_URL
+});
+
+redisClient.on('error', (err) => {
+    console.error("Redis error: ", err);
+});
+
+await redisClient.connect();
+
+export default redisClient;
